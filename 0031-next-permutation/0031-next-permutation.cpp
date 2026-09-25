1d9b779590f66b26d12af9bfc75736540430555e
Agni M N <165463111+Agnimn@users.noreply.github.com>
2026-09-25 07:55:55 +0530

    Time: 0 ms (100%), Space: 15.6 MB (98.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+    void nextPermutation(vector<int>& A) {
+
+        int n = A.size();
+        int pivot = -1;
+
+        // Step 1: Find the pivot
+        for(int i = n - 2; i >= 0; i--) {
+
+            if(A[i] < A[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        // If no pivot, array is in descending order
+        if(pivot == -1) {
+            reverse(A.begin(), A.end());
+            return;
+        }
+
+        // Step 2: Find the next larger element
+        for(int i = n - 1; i > pivot; i--) {
+
+            if(A[i] > A[pivot]) {
+                swap(A[i], A[pivot]);
+                break;
+            }
+        }
+
+        // Step 3: Reverse the suffix
+        int i = pivot + 1;
+        int j = n - 1;
+
+        while(i <= j) {
+            swap(A[i++], A[j--]);
+        }
     }
 };
